98333a4b72947a9e4238ff134a55e4fbb9b915cb
ashardianto <ashardian.to21@gmail.com>
2024-03-05 16:44:10 +0700

    feat: under construction for subjkt

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center items-center w-full h-screen">
+    <h1 class="text-center text-5xl">Under Construction...</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Silkscreen: [400, 700]
+    }
+  }
+})
