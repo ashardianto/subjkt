331f6746f19ef90c856812d3f58e82811250fe02
ashardianto <ashardian.to21@gmail.com>
2024-03-05 19:06:08 +0700

    fix: sbjkt gif bg

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center w-full h-screen bg-black">
-    <img class="pb-4" src="./assets/gif/sbjkt-gif.gif" />
+    <img class="pb-4" src="./assets/gif/subjkt.gif" />
     <h1 class="flex-col md:text-5xl text-center text-3xl text-white">Under Construction...</h1>
   </div>
 </template>
